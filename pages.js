30a7886131c82e3216fe2ca72dcf2d001965c470
JaeHyun-JOO-SKKU <dhdktltm117@naver.com>
2023-11-24 01:55:29 +0900

    assignment10

--- pages.js
+++ pages.js
@@ -1,6 +1,7 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/hashirshoaeb/hashirshoaeb.github.io.git";
+const repoURL =
+  "https://github.com/JaeHyun-JOO-SKKU/JaeHyun-JOO-SKKU.github.io.git";
 
 ghpages.publish(
   pathname,
