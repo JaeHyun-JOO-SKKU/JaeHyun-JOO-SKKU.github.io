b37a8987ceb5707dfd7b563368bcd01c06e3b9de
JaeHyun-JOO-SKKU <dhdktltm117@naver.com>
2023-11-24 21:26:37 +0900

    fixed

--- pages.js
+++ pages.js
@@ -1,7 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL =
-  "https://github.com/JaeHyun-JOO-SKKU/JaeHyun-JOO-SKKU.github.io.git";
+const repoURL = "https://github.com/JaeHyun-JOO-SKKU/JaeHyun-JOO-SKKU.github.io.git";
 
 ghpages.publish(
   pathname,
